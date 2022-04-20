6dbd3d59320e5ce9d04f6d9335886b7b2bb3cc87
vini3101 <vinigranatto@hotmail.com>
2022-04-20 18:09:00 -0300

    formatting files with prettier

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,16 +4,18 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.ocp.news/2016/04/onio-rings.jpg'),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://cdn.ocp.news/2016/04/onio-rings.jpg'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
