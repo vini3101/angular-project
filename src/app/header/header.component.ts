32d217dc0eb2569b5c74d1c6aa07895c811f917f
vini3101 <vinigranatto@hotmail.com>
2022-04-21 08:03:47 -0300

    feat: communicating the app component with the header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
-  ngOnInit(): void {}
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
