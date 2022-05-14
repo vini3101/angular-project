28adcc2ed77a2755ba581dafdb63f18bd9045a19
vini3101 <vinigranatto@hotmail.com>
2022-05-14 09:18:04 -0300

    feat: starting implementing the logic for edit mode

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { shoppingListService } from '../shopping-list.service';
@@ -9,11 +10,28 @@ import { shoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.scss'],
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
+  editedItemIndex!: number;
+  editMode = false;
+
   constructor(private shoppingListService: shoppingListService) {}
+
+  ngOnInit() {
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+      }
+    );
+  }
 
   onAddItem = (form: NgForm) =>
     this.shoppingListService.addIngredient(
       new Ingredient(form.value.name, form.value.amount)
     );
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -27,6 +27,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       );
   }
 
+  onEditItem(index: number) {
+    this.shoppingListService.startedEditing.next(index);
+  }
+
   ngOnDestroy() {
     this.ingredientsChanged.unsubscribe();
   }
