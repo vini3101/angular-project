cfc8bfc732a918ad037ff2e624a806b4235349b9
vini3101 <vinigranatto@hotmail.com>
2022-05-21 13:00:41 -0300

    feat: implemention of storage recipes method

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+  onSaveData() {
+    this.dataStorageService.storageRecipes();
+  }
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storageRecipes() {
+    this.http
+      .put(
+        'https://recipe-book-1b1f4-default-rtdb.firebaseio.com/recipes.json',
+        this.recipeService.getRecipes()
+      )
+      .subscribe();
+  }
 }
